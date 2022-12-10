3ed223fc654dade675fe3916e321833ea4da0606
vzotikas <vzotikas@vzotikas.com>
2022-12-10 16:39:30 +0100

    Add xcode project

---
+++ Faster/Faster/FasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FasterApp.swift
+//  Faster
+//
+//  Created by Administrator on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct FasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
